cbe30a5cdd1c9afb6559686967b52525bf81b3ed
Eric McKinney <ericmckinney.md@gmail.com>
2023-12-20 23:16:25 -0500

    Created Puzzle abstract class, main method

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+import sys
+from day_1 import day_1
+
+
 DAY = 1
+
+
+class Puzzle:
+    input: list[str]
+
+    def __init__(self, puzzle_input: list[str]):
+        self.input = puzzle_input
+
+    def solve(self) -> None:
+        print(f"Part 1: {self.part_1()}")
+        print(f"Part 2: {self.part_2()}")
+
+    def part_1(self) -> str:
+        raise NotImplementedError
+
+    def part_2(self) -> str:
+        raise NotImplementedError
 
 
 def read_puzzle_input() -> list[str]:
@@ -9,7 +30,17 @@ def read_puzzle_input() -> list[str]:
 
 
 def main():
-    pass
+    puzzle: Puzzle
+    p_input = read_puzzle_input()
+
+    match DAY:
+        case 1:
+            puzzle = day_1.Day1(p_input)
+        case _:
+            print("INVALID DAY", file=sys.stderr)
+            raise SystemExit
+
+    puzzle.solve()
 
 
 if __name__ == "__main__":
