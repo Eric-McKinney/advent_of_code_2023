4cbdc03fbf6e1f178daed23667c0b2b26b2556e6
Eric McKinney <ericmckinney.md@gmail.com>
2023-12-20 21:35:06 -0500

    Created function to read puzzle input

---
+++ main.py
@@ -0,0 +1,16 @@
+DAY = 1
+
+
+def read_puzzle_input() -> list[str]:
+    with open(f"day_{DAY}/input.txt", "r") as f:
+        p_input = f.readlines()
+
+    return p_input
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
