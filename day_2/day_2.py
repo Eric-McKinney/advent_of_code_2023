a1cb31fe59841010cec0c254eb89b8218ad66f5d
Eric McKinney <ericmckinney.md@gmail.com>
2023-12-21 22:10:03 -0500

    Started part 1

---
+++ day_2/day_2.py
@@ -0,0 +1,16 @@
+from main import Puzzle
+
+
+class Day2(Puzzle):
+    def part_1(self) -> str:
+        num_red = 12
+        num_green = 13
+        num_blue = 14
+
+        for line in self.input:
+            pass
+
+        return ""
+
+    def part_2(self) -> str:
+        return ""
