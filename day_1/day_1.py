0b2ae8b53212731cd8dae0f9487146360701e2c3
Eric McKinney <ericmckinney.md@gmail.com>
2023-12-21 21:52:55 -0500

    Finished part 2

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -24,6 +24,12 @@ class Day1(Puzzle):
             first_digit: str
             second_digit: str
 
+            first_digit = find_first_digit2(line)
+            second_digit = find_first_digit2(line[-1::-1], reverse=True)
+            calibration_num = int(first_digit + second_digit)
+
+            calibration_sum += calibration_num
+
         return str(calibration_sum)
 
 
@@ -31,3 +37,35 @@ def find_first_digit(line: str) -> str:
     for char in line:
         if char.isdigit():
             return char
+
+
+def find_first_digit2(line: str, reverse=False) -> str:
+    for idx, char in enumerate(line):
+        if char.isdigit():
+            return char
+
+        num = find_number_word(line[idx:idx+5], reverse)
+        if num != "":
+            return num
+
+
+def find_number_word(s: str, reverse: bool) -> str:
+    digits = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    }
+
+    for digit in digits:
+        if not reverse and digit in s[:len(digit)]:
+            return digits[digit]
+        elif reverse and digit[-1::-1] in s[:len(digit)]:
+            return digits[digit]
+
+    return ""
