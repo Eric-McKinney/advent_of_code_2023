8acb8849356480717bd214efb30f417a51708ea7
Eric McKinney <ericmckinney.md@gmail.com>
2023-12-23 13:37:38 -0500

    Started part 2

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -12,13 +12,13 @@ class Day2(Puzzle):
 
         for line in self.input:
             game_data = parse_line(line)
-            game_id = game_data[0]
+            game_id = int(game_data[0])
             valid_game = True
 
             for record in game_data[1:]:
                 for cube_count in record:
                     cube_count = cube_count.split()
-                    count = cube_count[0]
+                    count = int(cube_count[0])
                     color = cube_count[1]
 
                     if count > num_colors[color]:
@@ -31,12 +31,16 @@ class Day2(Puzzle):
         return str(valid_id_sum)
 
     def part_2(self) -> str:
-        return ""
+        power_sum = 0
+
+        for line in self.input:
+            game_data = parse_line(line)
 
 
 def parse_line(line: str) -> list[str]:
-    game_id = line[6]
-    game_records = line[9:-2]
+    line = line.split()
+    game_id = line[1][:-1]
+    game_records = " ".join(line[2:]).rstrip()
     game_records = game_records.split("; ")
 
     game_data = [game_id]
