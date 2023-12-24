f9ffa6fb1a748abb4a126e0a5cb957a3346c03c8
Eric McKinney <ericmckinney.md@gmail.com>
2023-12-24 12:54:02 -0500

    Part 1 still not correct

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -4,24 +4,48 @@ from puzzle_interface import Puzzle
 class Day3(Puzzle):
     def part_1(self) -> str:
         part_num_sum = 0
-        symbol_coordinates = []
 
+        num_str = ""
+        is_part_num = False
+        prev_y = 0
         for y, line in enumerate(self.input):
             for x, char in enumerate(line):
-                if is_symbol(char):
-                    symbol_coordinates.append((x, y))
+                if char.isdigit() and y == prev_y:
+                    num_str += char
 
-        for x, y in symbol_coordinates:
-            # same but check surrounding coords for digit before calling extract number on coords with digit
-            part_num_sum += extract_number(x, y)
+                    if self.has_symbol_adjacent(x, y):
+                        is_part_num = True
+                else:
+                    if num_str != "" and is_part_num:
+                        part_num_sum += int(num_str)
+
+                    num_str = ""
+                    is_part_num = False
+
+                prev_y = y
+
+        return str(part_num_sum)
 
     def part_2(self) -> str:
         pass
 
+    def has_symbol_adjacent(self, x: int, y: int) -> bool:
+        surrounding_coords = [(x - 1, y - 1), (x, y - 1), (x + 1, y - 1),
+                              (x - 1, y), (x + 1, y),
+                              (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)]
 
-def is_symbol(char: str):
+        for s_x, s_y in surrounding_coords:
+            if self.valid_coords(s_x, s_y) and is_symbol(self.input[s_y][s_x]):
+                return True
+
+        return False
+
+    def valid_coords(self, x: int, y: int) -> bool:
+        valid_x = 0 <= x < len(self.input[0])
+        valid_y = 0 <= y < len(self.input)
+
+        return valid_x and valid_y
+
+
+def is_symbol(char: str) -> bool:
     return not char.isdigit() and char != "."
-
-
-def extract_number(x: int, y: int) -> int:
-    pass
