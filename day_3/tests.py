02dba60ff7b2fa66094e0e7b3dbcd13dc8a80793
Eric McKinney <ericmckinney.md@gmail.com>
2023-12-29 13:39:04 -0500

    Initial commit

---
+++ day_3/tests.py
@@ -0,0 +1,11 @@
+import unittest
+from day_3 import Day3
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
