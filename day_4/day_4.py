d79acbaf0d3868874e119a23b675ec7442f88372
Eric McKinney <ericmckinney.md@gmail.com>
2024-01-01 02:34:22 -0500

    Finished attempt 1 of part 2, incorrect results

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -1,4 +1,5 @@
 from puzzle_interface import Puzzle
+from queue import Queue
 
 
 class Day4(Puzzle):
@@ -12,7 +13,22 @@ class Day4(Puzzle):
         return str(point_sum)
 
     def part_2(self) -> str:
-        pass
+        cards = parse_cards(self.input)
+        total_cards = 0
+
+        card_queue = Queue()
+        card_queue.put(0)
+        while not card_queue.empty():
+            current_card = card_queue.get()
+            num_winners = count_winning_nums(cards[current_card])
+            next_cards = [n for n in range(current_card + 1, current_card + num_winners + 1)]
+
+            for card in next_cards:
+                card_queue.put(card)
+
+            total_cards += 1
+
+        return str(total_cards)
 
 
 def parse_cards(cards: list[str]) -> list[dict[str, tuple[int, ...]]]:
@@ -49,10 +65,17 @@ def extract_nums(card: list[str]) -> tuple[tuple[int, ...], tuple[int, ...]]:
 
 
 def calculate_score(card: dict[str, tuple[int, ...]]) -> int:
+    num_winning_nums = count_winning_nums(card)
+    score = 0 if num_winning_nums == 0 else 2**(num_winning_nums - 1)
+
+    return score
+
+
+def count_winning_nums(card: dict[str, tuple[int, ...]]) -> int:
     num_winning_nums = 0
 
     for winning_num in card["winning_numbers"]:
         if winning_num in card["numbers"]:
             num_winning_nums += 1
 
-    return 2**(num_winning_nums - 1) if num_winning_nums != 0 else 0
+    return num_winning_nums
